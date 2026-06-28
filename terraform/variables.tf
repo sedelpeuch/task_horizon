756d7fecc4ffc00318fa78a91f4010598802b9ab
Sébastien Delpeuch <s.delpeuch@catie.fr>
2026-06-28 16:16:26 +0200

    Add Terraform configuration for AWS infrastructure setup
    - Created main.tf to define AWS provider and resources including VPC, subnets, S3 bucket, and RDS instance.
    - Added variables.tf to manage configuration variables such as bucket name, AWS region, and RDS credentials.

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default = "task-horizon-avatar-data"
+}
+
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
